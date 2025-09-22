e7e2fb42995ca8890d170498af4d35598f3eed31
Tiancheng Shao <39867814+tanxchan@users.noreply.github.com>
2025-09-22 16:46:27 -0700

    Updated descriptions
    Rewrote from scratch and added different projects.

---
+++ script.js
@@ -0,0 +1,13 @@
+
+let l = document.getElementsByClassName('language');
+for (let i = 0; i<l.length;i++){
+    l[i].addEventListener('click', ()=>{document.getElementsByClassName('languages')[0].scrollIntoView()})
+}
+let s = document.getElementsByClassName('data-sci');
+for (let i = 0; i<s.length;i++){
+    s[i].addEventListener('click', ()=>{document.getElementsByClassName('data-scis')[0].scrollIntoView()})
+}
+let b = document.getElementsByClassName('business');
+for (let i = 0; i<b.length;i++){
+    b[i].addEventListener('click', ()=>{document.getElementsByClassName('businesses')[0].scrollIntoView()})
+}
